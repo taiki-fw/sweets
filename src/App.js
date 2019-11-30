63b0a5c836fd994fd2996d931d507b611246e963
taiki-fw <taiki_52@outlook.jp>
2019-11-30 13:49:49 +0900

    ゲーム時間管理しているコンポーネントの導入

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { ActionBtn } from "./Btn";
+import { Timer } from "./Timer";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="actions">
-      <div className="actions__left">
-        <ActionBtn type="front">上</ActionBtn>
-        <div className="actions__left__center">
-          <ActionBtn type="left">左</ActionBtn>
-          <ActionBtn type="right">右</ActionBtn>
+    <React.Fragment>
+      <Timer />
+      <div className="actions">
+        <div className="actions__left">
+          <ActionBtn type="front">上</ActionBtn>
+          <div className="actions__left__center">
+            <ActionBtn type="left">左</ActionBtn>
+            <ActionBtn type="right">右</ActionBtn>
+          </div>
+          <ActionBtn type="back">下</ActionBtn>
         </div>
-        <ActionBtn type="back">下</ActionBtn>
+        <div className="actions__right">
+          <ActionBtn type="up">Up</ActionBtn>
+          <ActionBtn type="down">Down</ActionBtn>
+        </div>
       </div>
-      <div className="actions__right">
-        <ActionBtn type="up">Up</ActionBtn>
-        <ActionBtn type="down">Down</ActionBtn>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
